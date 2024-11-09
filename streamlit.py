a82bada88052479641f130470b242585cac9ae52
mj <47093544+aesodapop@users.noreply.github.com>
2024-11-09 13:23:15 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 
 # Function to calculate 401(k) contributions
@@ -5,10 +6,10 @@ def calculate_401k_contributions(
     base_salary, aip_april, aip_october, age,
     pre_tax_percentage, roth_percentage, after_tax_percentage
 ):
-    salary_cap = 345000  # IRS salary cap for 401(k) contributions
-    annual_pre_tax_roth_limit = 23000  # IRS limit for combined pre-tax and Roth contributions
+    salary_cap = 350000  # IRS salary cap for 401(k) contributions
+    annual_pre_tax_roth_limit = 23500  # IRS limit for combined pre-tax and Roth contributions
     catch_up_limit = 7500  # Catch-up contribution limit for age 50+
-    contribution_limit = 69000  # Total contribution limit, including employee and employer contributions
+    contribution_limit = 70000  # Total contribution limit, including employee and employer contributions
     company_match_limit_percentage = 5 / 100  # Company match percentage
     pay_periods = 26  # Number of pay periods
 
@@ -176,83 +177,170 @@ def calculate_401k_contributions(
 
     return (
         breakdown, total_pre_tax, total_roth, total_pre_tax_catch_up, total_roth_catch_up,
-        total_company_match, total_after_tax, total_contributions, estimated_true_up
+        total_company_match, total_after_tax, total_contributions, estimated_true_up,
+        annual_pre_tax_roth_limit, catch_up_limit, contribution_limit
     )
+
 
 # Streamlit App
 def main():
-    st.title("401(k) Contribution Calculator")
+    
+    st.title(":green[2025 401(k) Contribution Calculator]")
+    st.subheader("Estimate your annual contribtions and your contributions per pay period to help determine your contribution percentages.")
 
     errors = []
 
-    # Input fields
-    base_salary = st.number_input("Enter your base salary:", min_value=0.0, value=50000.0, step=1000.0, format="%0.0f")
-    age = st.number_input("Enter your age:", min_value=0, value=30, step=1)
-    aip_april = st.number_input("Enter your expected AIP for April:", min_value=0.0, value=5000.0, step=500.0, format="%0.0f")
-    aip_october = st.number_input("Enter your expected AIP for October:", min_value=0.0, value=2000.0, step=500.0, format="%0.0f")
-    pre_tax_percentage = st.number_input("Enter your pre-tax contribution percentage:", min_value=0.0, max_value=75.0, value=5.0, step=5.0, format="%0.0f")
-    roth_percentage = st.number_input("Enter your Roth contribution percentage:", min_value=0.0, max_value=75.0, value=10.0, step=5.0, format="%0.0f")
-    after_tax_percentage = st.number_input("Enter your after-tax contribution percentage:", min_value=0.0, max_value=75.0,step=5.0, value=15.0, format="%0.0f")
+    col1, col3, col2 = st.columns([2.5, 0.5, 3])
 
+    with col1:
+        #Base salary input box
+        base_salary = st.text_input('Enter your base salary', placeholder='e.g. 100000')
+        if base_salary != '':
+            try:
+                base_salary = int(base_salary)
+            except ValueError:  
+                st.markdown(f"**:red[{base_salary}]** Please input an integer.")
+        else:
+            base_salary = 0
+
+        #Age input box
+        age = st.text_input('Enter your age', placeholder='e.g. 37')
+        if age != '':
+            try:
+                age = int(age)
+            except ValueError:  
+                st.markdown(f"**:red[{age}]** Please input an integer.")
+    
+        #1st bonus input box
+        aip_april = st.text_input('Enter your 1st half of the year bonus', placeholder='e.g. 5000')
+        if aip_april != '':
+            try:
+                aip_april = int(aip_april)
+            except ValueError:
+                st.markdown(f"**:red[{aip_april}]** Please input an integer.")
+
+        #2nd bonus input box
+        aip_october = st.text_input('Enter your 2nd half of the year bonus', placeholder='e.g. 2000')
+        if aip_october != '':
+            try:
+                aip_october = int(aip_october)
+            except ValueError:  
+                st.markdown(f"**:red[{aip_october}]** Please input an integer.")
+            
+        #PT input box
+        pre_tax_percentage = st.text_input('Enter your pre-tax contribution percentage', placeholder='e.g. 5')
+        if pre_tax_percentage != '':
+            try:
+                pre_tax_percentage = int(pre_tax_percentage)
+            except ValueError:  
+                st.markdown(f"**:red[{pre_tax_percentage}]** Please input an integer.")
+            
+        #Roth input box
+        roth_percentage = st.text_input('Enter your Roth contribution percentage', placeholder='e.g. 10')
+        if roth_percentage != '':
+            try:
+                roth_percentage = int(roth_percentage)
+            except ValueError:  
+                st.markdown(f"**:red[{roth_percentage}]** Please input an integer.")
+            
+        #AT input box
+        after_tax_percentage = st.text_input('Enter your after-tax contribution percentage', placeholder='e.g. 15')
+        if after_tax_percentage != '':
+            try:
+                after_tax_percentage = int(after_tax_percentage)
+            except ValueError:  
+                st.markdown(f"**:red[{after_tax_percentage}]** Please input an integer.")
+            
     # Check total contribution percentage input does not exceed 75%
-    total_percentage = pre_tax_percentage + roth_percentage + after_tax_percentage
-    if total_percentage > 75:
-        st.error("Error: Please adjust your contribution percentages to not exceed 75%.")
-        return
+    try:
+        total_percentage = pre_tax_percentage + roth_percentage + after_tax_percentage
+    except TypeError:
+        print("Type Error")
+    
+    try:
+        if total_percentage > 75:
+            st.error("Error: Please adjust your contribution percentages to not exceed 75%.")
+            return
+    except TypeError:
+        print("Type Error 2")
+    except UnboundLocalError:
+        print("Unbound Local Error")
 
+    with col3:
+        print("")
+        
     # Calculate and display results when the button is clicked
     if st.button("Calculate"):
-        try:
-            (
-                breakdown, total_pre_tax, total_roth, total_pre_tax_catch_up, total_roth_catch_up,
-                total_company_match, total_after_tax, total_contributions, estimated_true_up
-            ) = calculate_401k_contributions(
-                base_salary, aip_april, aip_october, age,
-                pre_tax_percentage, roth_percentage, after_tax_percentage
-            )
+        with col2:
+            try:
+                (
+                    breakdown, total_pre_tax, total_roth, total_pre_tax_catch_up, total_roth_catch_up,
+                    total_company_match, total_after_tax, total_contributions, estimated_true_up,
+                    annual_pre_tax_roth_limit, catch_up_limit, contribution_limit
+                ) = calculate_401k_contributions(
+                    base_salary, aip_april, aip_october, age,
+                    pre_tax_percentage, roth_percentage, after_tax_percentage
+                )
 
-            if not breakdown:
-                st.warning("No contributions were made based on the input percentages.")
-                return
-            
-            st.subheader("Total Annual Contributions:")
-            st.write(f"  Pre-tax contributions: ${total_pre_tax:,.2f}")
-            st.write(f"  Roth contributions: ${total_roth:,.2f}")
-            if age >= 50:
-                st.write(f"  Pre-tax catch-up contributions: ${total_pre_tax_catch_up:,.2f}")
-                st.write(f"  Roth catch-up contributions: ${total_roth_catch_up:,.2f}")
-            else:
-                st.write("  Catch-up contributions: N/A")
-            st.write(f"  Company match: ${total_company_match:,.2f}")
-            st.write(f"  After-tax contributions: ${total_after_tax:,.2f}")
-            st.write(f"  Total contributions: ${total_contributions:,.2f}")
-            st.write(f"  Estimated True-Up: ${estimated_true_up:,.2f}")
+                if not breakdown:
+                    st.warning("No contributions were made based on the input percentages.")
+                    return
 
-            st.write("---")
-            
-            st.subheader("Detailed Breakdown of Contributions per Pay Period:")
-            for row in breakdown:
-                st.write(f"**Pay Period {row['period']}**")
-                if row['period'] == 8:
-                    st.write("  *AIP added to base salary in April*")
-                elif row['period'] == 21:
-                    st.write("  *AIP added to base salary in October*")
-                st.write(f"  Salary for period: ${row['salary_per_period']:,.2f}")
-                st.write(f"  Pre-tax: ${row['pre_tax']:,.2f}")
-                st.write(f"  Roth: ${row['roth']:,.2f}")
-                if age >= 50:
-                    st.write(f"  Pre-tax catch-up: ${row['pre_tax_catch_up']:,.2f}")
-                    st.write(f"  Roth catch-up: ${row['roth_catch_up']:,.2f}")
-                st.write(f"  Company match: ${row['company_match']:,.2f}")
-                st.write(f"  After-tax: ${row['after_tax']:,.2f}")
-                st.write(f"  Total contributions as of this period: ${row['total_contributions']:,.2f}")
-
-                # Display any limit messages for this period
-                for message in row['limit_messages']:
-                    st.warning(f"  **{message}**")
-
+                st.markdown("**Your Annual Contribution Limits**")
+                st.markdown(f"  Pre-tax/Roth Limit: :green[${annual_pre_tax_roth_limit:,.0f}]")
+                st.markdown(f"  Catch-Up: :green[${catch_up_limit:,.0f}]")
+                st.markdown(f"  Total: :green[${contribution_limit:,.0f}]")
+                
                 st.write("---")
+                
+                st.markdown("**Your Total Annual Contributions**")
+                st.markdown(f"  Pre-tax: :green[${total_pre_tax:,.2f}]")
+                st.markdown(f"  Roth: :green[${total_roth:,.2f}]")
+                if age >= 50:
+                    st.markdown(f"  Pre-tax catch-up: :green[${total_pre_tax_catch_up:,.2f}]")
+                    st.markdown(f"  Roth catch-up: :green[${total_roth_catch_up:,.2f}]")
+                else:
+                    st.markdown("  Catch-up contributions: N/A")
+                st.markdown(f"  Company match: :green[${total_company_match:,.2f}]")
+                st.markdown(f"  After-tax: :green[${total_after_tax:,.2f}]")
+                st.markdown(f"  Total (not including Estimated True-Up): :green[${total_contributions:,.2f}]")
+                st.markdown(f"  Estimated True-Up: :green[${estimated_true_up:,.2f}]")
 
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
+        st.markdown("""
+            <style>
+                [data-testid="column"]:nth-child(2){
+                    background-color: lightgrey;
+                }
+            </style>
+            """, unsafe_allow_html=True
+        )
+
+        try:
+                st.subheader("Breakdown of Your Contributions per Pay Period")
+                for row in breakdown:
+                    st.markdown(f"**Pay Period :green[{row['period']}]**")
+                    if row['period'] == 8:
+                        st.markdown("  :red[*1st half of the year bonus added to your wages this pay period*]")
+                    elif row['period'] == 21:
+                        st.markdown("  :red[*2nd half of the year bonus added to your wages this pay period*]")
+                    st.markdown(f"  Wages this pay period: :green[${row['salary_per_period']:,.2f}]")
+                    st.markdown(f"  Pre-tax: :green[${row['pre_tax']:,.2f}]")
+                    st.markdown(f"  Roth: :green[${row['roth']:,.2f}]")
+                    if age >= 50:
+                        st.markdown(f"  Pre-tax catch-up: :green[${row['pre_tax_catch_up']:,.2f}]")
+                        st.markdown(f"  Roth catch-up: :green[${row['roth_catch_up']:,.2f}]")
+                    st.markdown(f"  Company match: :green[${row['company_match']:,.2f}]")
+                    st.markdown(f"  After-tax: :green[${row['after_tax']:,.2f}]")
+                    st.markdown(f"  Total contributions as of this period: :green[${row['total_contributions']:,.2f}]")
+
+                    # Display any limit messages for this period
+                    for message in row['limit_messages']:
+                        st.warning(f"  **{message}**")
+
+                    st.write("---")
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
