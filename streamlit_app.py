f5791dd061d248b570e43a18ce06ae551a9af5ef
mj <47093544+aesodapop@users.noreply.github.com>
2025-12-08 12:38:38 -0600

    Initialize Streamlit app with navigation and pages

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+about = st.Page("pages/about.py", title="About", icon=":material/dashboard:", default=True)
+gallery = st.Page("pages/gallery.py", title="Gallery", icon=":material/gallery:", default=False)
+
+pg = st.navigation({"Cheval": [about, gallery]})
+
+pg.run()
