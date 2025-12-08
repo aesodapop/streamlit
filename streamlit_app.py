c87cff0106418fe451bd0bcffee251ec7a156e34
mj <47093544+aesodapop@users.noreply.github.com>
2025-12-08 13:40:15 -0600

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-about = st.Page("pages/about.py", title="About", icon=":material/about:", default=True)
-gallery = st.Page("pages/gallery.py", title="Gallery", icon=":material/gallery:", default=False)
-
-pg = st.navigation({"Cheval": [about, gallery]})
-
-pg.run()
